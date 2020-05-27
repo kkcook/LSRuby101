2920acbb593726cf7bdc95fd6697054188b1fe98
Karissa Cook <karissa.cook@me.com>
2020-05-26 19:26:05 -0700

    Adding PEDAC for doubled_numbers.rb exercise

---
+++ lesson_4/doubled_numbers.rb
@@ -0,0 +1,8 @@
+# We want to double the numbers by mutating the original array
+# Input array, output array
+# Edge cases are 0, input is assumed to be an integer
+# Data structure is array
+# Iterate through array with loop
+# Going to set each element of the input array to current number variable
+# Going to set array element to equal 2 * current number
+# Perform task until count == array.size

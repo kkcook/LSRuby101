e96c9d5831ffc8236e041da0b4e99c758750c334
Karissa Cook <karissa.cook@me.com>
2020-06-05 11:25:04 -0700

    Redid How Many? exercise

--- small_problems/easy1/easy_1_prob_4.rb
+++ small_problems/easy1/easy_1_prob_4.rb
@@ -1,25 +1,39 @@
+# frozen_string_literal: true
+
 # Prob: write a method that counts the number of occurrences of each element in given array
 # Input: array, output: hash
 # Edge cases: empty array...
 # Datastructure: array to hash
 # Algorithm: iterate through each and see if key exists, if so, add one to count, otherwise create key and set value to 1
 
-vehicles = [
-    'car', 'car', 'truck', 'car', 'SUV', 'truck',
-    'motorcycle', 'motorcycle', 'car', 'truck'
-  ]
+vehicles = %w[
+  car car truck car SUV truck
+  motorcycle motorcycle car truck
+]
 
-  def count_occurrences(list)
+#   def count_occurrences(list)
+#     output = {}
+#     list.each do |element|
+#         if output[element]
+#             output[element] += 1
+#         else
+#             output[element] = 1
+#         end
+#     end
+#
+#     output
+#   end
+
+def count_occurrences(list)
     output = {}
-    list.each do |element|
-        if output[element]
-            output[element] += 1
-        else
-            output[element] = 1
-        end
+
+    list.uniq.each do |element|
+        output[element] = list.count(element)
     end
 
-    output
-  end
-  
-  p count_occurrences(vehicles)
+    output.each do |key, value|
+        puts "#{key} => #{value}"
+    end
+end
+
+count_occurrences(vehicles)

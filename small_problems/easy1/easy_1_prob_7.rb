a22570db0801e67e4f28de334054191597e2f249
Karissa Cook <karissa.cook@me.com>
2020-06-05 13:52:38 -0700

    Changed algorithm on problem 7

--- small_problems/easy1/easy_1_prob_7.rb
+++ small_problems/easy1/easy_1_prob_7.rb
@@ -7,6 +7,7 @@
 # A: int.times if odd will end in 1 if even will end in 0
 #       If odd, minus 1 / 2 print "10"'s, if even / 2 print "10"s
 
+=begin FIRST PASS
 def stringy(num)
   output = ''
   if num.odd?
@@ -16,12 +17,32 @@ def stringy(num)
     output << '10' * (num/2)
   end
 end
+=end
 
-puts stringy(10)
-puts stringy(1)
+# New algorithm idea: print 1 or zero based on odd/evenness of the index
+
+def stringy(num)
+    output = ''
+    
+    num.times do |x|
+        if x.even? 
+            output << '1'
+        else
+            output << '0'
+        end
+    end
+
+    output
+end
+
 
 # Examples
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+# Extra tests
+puts stringy(10)
+puts stringy(1)
+puts stringy(0)

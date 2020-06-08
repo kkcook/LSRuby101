bfc6e1425aa2137697197e1798b366c0e5fe78d7
Karissa Cook <karissa.cook@me.com>
2020-06-08 13:11:44 -0700

    First attempt prob 10

---
+++ small_problems/easy1/easy_1_prob_10.rb
@@ -0,0 +1,18 @@
+# Bonus Calc
+# Prb: write a method that takes an int and a bool, 
+#   if bool is true the return val should be half of int arg
+# Examples below
+# Datastructure: integer math
+# A: check if bool is true, return half of int if yes and 0 if false
+
+def calculate_bonus(salary, receives_bonus)
+  return (salary / 2) if receives_bonus == true
+  0
+end
+
+
+
+# Examples
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000

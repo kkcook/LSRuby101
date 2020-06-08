36b908e8ad0a78663ca941e3f221fb1cc7c5e852
Karissa Cook <karissa.cook@me.com>
2020-06-08 14:35:39 -0700

    PEDAC outline

---
+++ small_problems/easy2/4_when_retire.rb
@@ -0,0 +1,10 @@
+# When will I retire?
+
+# Prob: build a prog that asks age, desired retirement
+#   year, and then outputs how long they have to work
+# Input/output: ints
+# Example given on LS
+# Datastructure: integer math
+# Alg: ask for age, then age of retirement, then display
+#   "It's 2016. You will retire in 2056."
+#   "You have only 40 years of work to go!"

068b0e60d61e928803c1a069eae0c674450d9476
Karissa Cook <karissa.cook@me.com>
2020-06-08 14:43:48 -0700

    Completed with Time.now

--- small_problems/easy2/4_when_retire.rb
+++ small_problems/easy2/4_when_retire.rb
@@ -8,3 +8,16 @@
 # Alg: ask for age, then age of retirement, then display
 #   "It's 2016. You will retire in 2056."
 #   "You have only 40 years of work to go!"
+
+puts "=> How old are you?"
+age = gets.chomp.to_i
+
+puts "=> What age do you want to retire at?"
+retirement_age = gets.chomp.to_i
+
+year = Time.now.year
+years_to_retirement = retirement_age - age
+retirement_year = year + years_to_retirement
+
+puts "It's' #{year}. You will retire in #{retirement_year}."
+puts "You have only #{years_to_retirement} years to go!"

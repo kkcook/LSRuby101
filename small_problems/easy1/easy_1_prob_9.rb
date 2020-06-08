76825a4d08a60d775ce946a818696fee23226683
Karissa Cook <karissa.cook@me.com>
2020-06-08 13:11:36 -0700

    First round of prob 9 complete

--- small_problems/easy1/easy_1_prob_9.rb
+++ small_problems/easy1/easy_1_prob_9.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 # Sum of Digits
 
-# Problem
+# Problem: write a method that takes an argument, a pos int, and returns the sum of its digits
+# Examples below
+# Datastructures: ints
+# Alg: split num into digits via string/chars, turn back into nums, sum
+
+def sum(number)
+  number.to_s.chars.map(&:to_i).reduce(:+)
+end
+
+# Examples
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45

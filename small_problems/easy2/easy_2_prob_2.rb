d1d8d3ef92050af87f9072fa6593353de0741b01
Karissa Cook <karissa.cook@me.com>
2020-06-08 13:48:31 -0700

    Further exploration on area calculator

--- small_problems/easy2/easy_2_prob_2.rb
+++ small_problems/easy2/easy_2_prob_2.rb
@@ -1,17 +1,28 @@
+# frozen_string_literal: true
+
 # How big is the room?
-# Build a program that asks a user for the length and width of a room in meters 
+# Build a program that asks a user for the length and width of a room in meters
 #   and then displays the area of the room in both square meters and square feet.
 # Note: 1 square meter == 10.7639 square feet
 # Input/output: ints
 # Datastructure: integer math
 
-puts "Enter the length of the room in meters:"
-length = gets.chomp.to_i
+# Modify this program to ask for the input measurements in feet,
+#   and display the results in square feet, square inches, and square centimeters.
 
-puts "Enter the width of the room in meters:"
-width = gets.chomp.to_i
+SQFEET_TO_SQINCHES = 144
+SQFEET_TO_CMSQ = 929.03
 
-area = length * width
-imperial_area = (area * 10.7639).to_i
+puts 'Enter the length of the room in feet:'
+length = gets.chomp.to_f
 
-puts "The area is #{area} square meters (#{imperial_area} square feet)."
+puts 'Enter the width of the room in feet:'
+width = gets.chomp.to_f
+
+square_feet = length * width
+square_inches = square_feet * SQFEET_TO_SQINCHES
+cm_square = square_feet * SQFEET_TO_CMSQ
+
+puts "The area is #{square_feet.round(2)} square feet, " \
+     "#{square_inches.round(2)} square inches, and " \
+     "#{cm_square.round(2)} centimeters squared."

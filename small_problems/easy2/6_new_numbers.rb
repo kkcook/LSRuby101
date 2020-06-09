6c75b359087bf2ad219b4184f61b01bec641c193
Karissa Cook <karissa.cook@me.com>
2020-06-08 19:34:31 -0700

    First pass at new numbers

---
+++ small_problems/easy2/6_new_numbers.rb
@@ -0,0 +1,8 @@
+# Odd Numbers
+# P: Print all odd numbers from 1 to 99, inclusive. 
+#   All numbers should be printed on separate lines.
+# Examples: n/a
+# Datastructure: loops
+# Algorithm: for 0..100 puts if odd
+
+100.times {|x| puts x if x.odd?}

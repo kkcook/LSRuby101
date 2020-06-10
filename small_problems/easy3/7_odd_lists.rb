8162d84bb3277f2f04c26bc24f70ba1c18f697f3
Karissa Cook <karissa.cook@me.com>
2020-06-09 19:03:35 -0700

    pedac odd lists

---
+++ small_problems/easy3/7_odd_lists.rb
@@ -0,0 +1,16 @@
+# Odd Lists
+# P: write method that takes every other val from array and returns it
+# D: arrays
+# A: create new array based on index of input array
+
+
+
+# Examples:
+puts oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+puts oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+puts oddities(['abc', 'def']) == ['abc']
+puts oddities([123]) == [123]
+puts oddities([]) == []
+
+
+

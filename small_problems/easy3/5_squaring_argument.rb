2625cc63aab6228db085b2eda845d0c68942c15e
Karissa Cook <karissa.cook@me.com>
2020-06-09 18:48:07 -0700

    Further exploration with powers

--- small_problems/easy3/5_squaring_argument.rb
+++ small_problems/easy3/5_squaring_argument.rb
@@ -1,8 +1,21 @@
 # Write method that squares an argument
 # Example: square(-8) == 64
 
+def multiply(a, b)
+  a * b
+end
+
 def square(num)
-  num ** 2
+  multiply(num, num)
+end
+
+def power(num, power)
+  if power == 0
+    return 1
+  else
+    num * (power(num, power - 1))
+  end
 end
 
 puts square(-8) == 64
+puts power(2, 3)

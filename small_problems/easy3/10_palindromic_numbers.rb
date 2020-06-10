db2ddf364a014b9e3b64d7daa1cb0f6dda424707
Karissa Cook <karissa.cook@me.com>
2020-06-10 14:38:56 -0700

    Solved another way with strings

--- small_problems/easy3/10_palindromic_numbers.rb
+++ small_problems/easy3/10_palindromic_numbers.rb
@@ -1,19 +1,18 @@
+# frozen_string_literal: true
+
 # Palindromic Numbers
 
 # P: Write a program that tells if number is palindrome
 # Examples at bottom
-# D: arrays
+# D: arrays used first, then strings was faster and cleaner
 # A: turn number into array, see if reverse is ==
 
 def palindromic_number?(number)
-  num_array = number.digits
-  num_array == num_array.reverse
+  number.to_s == number.to_s.reverse
 end
 
-
-
 # Examples:
-puts palindromic_number?(34543) == true
-puts palindromic_number?(123210) == false
+puts palindromic_number?(34_543) == true
+puts palindromic_number?(123_210) == false
 puts palindromic_number?(22) == true
 puts palindromic_number?(5) == true

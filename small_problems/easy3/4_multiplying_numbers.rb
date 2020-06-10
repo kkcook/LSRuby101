7c63322da936fb012f88593f9d9ee6b494b4319c
Karissa Cook <karissa.cook@me.com>
2020-06-09 18:29:02 -0700

    First pass at 4

---
+++ small_problems/easy3/4_multiplying_numbers.rb
@@ -0,0 +1,10 @@
+# Multiplying two numbers
+# Problem: write a method for multiplying two nums
+# Ex: multiply(5, 3) == 15
+# DS: integer math
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(5, 3) == 15

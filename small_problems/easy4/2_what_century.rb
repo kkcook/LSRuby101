f611849dfe0bc7855f3a24c258a31fbd987a9152
Karissa Cook <karissa.cook@me.com>
2020-06-10 16:06:30 -0700

    First cases working, last three not

--- small_problems/easy4/2_what_century.rb
+++ small_problems/easy4/2_what_century.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # What Century is That?
 
 # P: write a method that takes a year and returns century
@@ -6,26 +8,25 @@
 # A: see if number ends in 0, divide by 100, associate suffix by last digit from hash
 
 def century(year = 1)
-  suffixes = {0 => 'th', 1 => 'st', 2 => 'nd', 3 => 'rd', 4 => 'th', 5 => 'th', 6 => 'th', 7 => 'th', 8 => 'th', 9 => 'th'}
-  cent_num = 0
-  if year % 100 == 0
-    cent_num = year / 100
-  else
-    cent_num = (year / 100) + 1
-  end
-  cent_num.to_s
+  suffixes = { 0 => 'th', 1 => 'st', 2 => 'nd', 3 => 'rd', 4 => 'th', 5 => 'th', 6 => 'th', 7 => 'th', 8 => 'th', 9 => 'th' }
+
+  century = if year % 100 == 0
+               year / 100
+             else
+               (year / 100) + 1
+             end
+
+  last_digit = century.digits.first
+  century.to_s + suffixes[last_digit]
 end
 
-puts century(2010)
-puts century(1000)
-
 # Examples
-# century(2000) == '20th'
-# century(2001) == '21st'
-# century(1965) == '20th'
-# century(256) == '3rd'
-# century(5) == '1st'
-# century(10103) == '102nd'
-# century(1052) == '11th'
-# century(1127) == '12th'
-# century(11201) == '113th'
+puts century(2000) == '20th'
+puts century(2001) == '21st'
+puts century(1965) == '20th'
+puts century(256) == '3rd'
+puts century(5) == '1st'
+puts century(10103) == '102nd'
+puts century(1052) #== '11th'
+puts century(1127) #== '12th'
+puts century(11201) #== '113th'

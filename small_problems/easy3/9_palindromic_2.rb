93aaeb5248225ea4ce432b5e3654695731b87351
Karissa Cook <karissa.cook@me.com>
2020-06-09 19:53:57 -0700

    completed with help - review this

---
+++ small_problems/easy3/9_palindromic_2.rb
@@ -0,0 +1,29 @@
+# Palindromic Strings pt. 2
+
+# P: write a method that finds palindromes without
+#   worrying about case or punctuation
+# Examples at bottom
+# Datastructure: array
+# Alg: convert string to array, getting rid of all non
+#   numbers or letters...
+
+def palindrome?(word)
+  word == word.reverse
+end
+
+def real_palindrome?(input)
+  new_string = input.downcase.delete('^a-z0-9')
+  palindrome?(new_string)
+end
+
+
+
+
+
+# Examples: 
+p real_palindrome?('madam') == true
+puts real_palindrome?('Madam') == true           # (case does not matter)
+puts real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+puts real_palindrome?('356653') == true
+puts real_palindrome?('356a653') == true
+puts real_palindrome?('123ab321') == false

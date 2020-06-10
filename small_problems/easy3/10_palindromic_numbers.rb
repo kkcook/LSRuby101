0c296548884b8016200f75ca13b92668ff62829f
Karissa Cook <karissa.cook@me.com>
2020-06-10 14:32:51 -0700

    PEDAC of palindromic numbers

---
+++ small_problems/easy3/10_palindromic_numbers.rb
@@ -0,0 +1,16 @@
+# Palindromic Numbers
+
+# P: Write a program that tells if number is palindrome
+# Examples at bottom
+# D: arrays
+# A: turn number into array, see if reverse is ==
+
+
+
+
+
+# Examples:
+puts palindromic_number?(34543) == true
+puts palindromic_number?(123210) == false
+puts palindromic_number?(22) == true
+puts palindromic_number?(5) == true

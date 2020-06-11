1f9e5f5f672b0746e39b94acd60ff5e2b141cbdf
Karissa Cook <karissa.cook@me.com>
2020-06-11 13:29:03 -0700

    Solved further exploration with inject

--- small_problems/easy4/6_running_totals.rb
+++ small_problems/easy4/6_running_totals.rb
@@ -15,13 +15,17 @@
 # end
 
 # Further Exploration: solve with Enumerable#each_with_object or Enumerable#inject
-def running_total(input)
-  sum = 0
-  input.each_with_object([]) { |num, array| array << (sum += num)}
+# def running_total(input)
+#   sum = 0
+#   input.each_with_object([]) { |num, array| array << (sum += num)}
+# end
+
+def running_total(array)
+  (0..array.size - 1).map { |i| array[0..i].inject(:+)}
 end
 
 # Examples
-p running_total([2, 5, 13]) == [2, 7, 20]
-p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-p running_total([3]) == [3]
-p running_total([]) == []
+p running_total([2, 5, 13]) #== [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) #== [14, 25, 32, 47, 67]
+p running_total([3]) #== [3]
+p running_total([]) #== []

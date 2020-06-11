a3a8d114308034bb295c69c318160911c0f8ca0d
Karissa Cook <karissa.cook@me.com>
2020-06-10 22:05:20 -0700

    PEDAC completed

---
+++ small_problems/easy4/5_multiples_3_5.rb
@@ -0,0 +1,19 @@
+# Multiples of 3 and 5
+
+# Write a program that returns the sume of all multiples of 
+# 3 and 5 between 1 and a supplied pos int greater than 1
+
+# Input int output int
+# Examples at bottom
+# Datastructure: int -> array -> int
+# Alg: append all evenly dividing factors into an array
+#  Return the sum of that aray
+
+
+
+
+# Examples:
+puts multisum(3) == 3
+puts multisum(5) == 8
+puts multisum(10) == 33
+puts multisum(1000) == 234168

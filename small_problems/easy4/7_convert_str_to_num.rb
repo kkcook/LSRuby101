1d4b6aa8dd007762bf63a3d3e4ae1e550658d842
Karissa Cook <karissa.cook@me.com>
2020-06-11 13:51:39 -0700

    PEDAC completed

---
+++ small_problems/easy4/7_convert_str_to_num.rb
@@ -0,0 +1,11 @@
+# Convert String to Number
+# Prob: convert string to number without .to_i
+# Examples at bottom
+# Datastructure: searching of some kind, arrays
+# Alg: get character id of string character, replace with int
+
+
+
+# Examples
+p string_to_integer('4321') == 4321
+p string_to_integer('570') == 570

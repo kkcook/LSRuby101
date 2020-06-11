1dc484714af6755399f1851536918a23ff57e167
Karissa Cook <karissa.cook@me.com>
2020-06-11 13:09:12 -0700

    First working solution found

--- small_problems/easy4/6_running_totals.rb
+++ small_problems/easy4/6_running_totals.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Running Totals
 # P: write method that takes array and returns array of same size
 #   returned array should display running total sum from supplied array
@@ -8,22 +10,19 @@
 #   going to start with sum = first element in array, each iteration is += next element
 
 def running_total(input_array)
-  sum = input_array.first
+  sum = 0
   output_array = []
 
   input_array.each do |num|
-
+    sum += num
+    output_array << sum
   end
+
+  output_array
 end
 
-
-
-
-
-
-
 # Examples
-puts running_total([2, 5, 13]) == [2, 7, 20]
-puts running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-puts running_total([3]) == [3]
-puts running_total([]) == []
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []

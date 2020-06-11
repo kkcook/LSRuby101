786e9fc0d1e84e9f1e4a98fb7af39d3eb527a4dc
Karissa Cook <karissa.cook@me.com>
2020-06-10 22:34:01 -0700

    Got rid of needless array step

--- small_problems/easy4/5_multiples_3_5.rb
+++ small_problems/easy4/5_multiples_3_5.rb
@@ -1,31 +1,27 @@
+# frozen_string_literal: true
+
 # Multiples of 3 and 5
 
-# Write a program that returns the sume of all multiples of 
+# Write a program that returns the sume of all multiples of
 # 3 and 5 between 1 and a supplied pos int greater than 1
 
 # Input int output int
-# Examples at bottom
+# Examples at bottom, added 1
 # Datastructure: int -> array -> int
 # Alg: append all evenly dividing factors into an array
 #  Return the sum of that aray
 
 def multisum(num)
-  factor_array = []
   sum = 0
 
-  (3..num).map do |num|
-    if num % 3 == 0 || num % 5 == 0
-      factor_array << num
-    end
+  1.upto(num) do |num|
+    sum += num if num % 3 == 0 || num % 5 == 0
   end
-
-  factor_array.each {|factor| sum += factor}
-  sum
 end
-
 
 # Examples:
 puts multisum(3) == 3
 puts multisum(5) == 8
 puts multisum(10) == 33
-puts multisum(1000) == 234168
+puts multisum(1000) == 234_168
+puts multisum(1)

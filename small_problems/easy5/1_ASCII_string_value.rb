8fec85d34492bf5973ec24af9851f859d4e8db07
Karissa Cook <karissa.cook@me.com>
2020-06-12 16:29:56 -0700

    PEDAC and examples outlined

---
+++ small_problems/easy5/1_ASCII_string_value.rb
@@ -0,0 +1,16 @@
+# ASCII String Value
+
+# P: write a method that determines and returns ASCII string
+#   value of a string that is passed in as an arg
+# E: examples at bottom, look out for empty string
+# D: array
+# A: make string into an array, calc each ASCII val, sum, return
+
+
+
+
+# Examples:
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0

a33a16ecd8e295a87b258fcb764500825f00cc00
Karissa Cook <karissa.cook@me.com>
2020-06-11 17:54:15 -0700

    Cleaned it up with case statement after seeing ans

--- small_problems/easy4/8_convert_str_to_signed.rb
+++ small_problems/easy4/8_convert_str_to_signed.rb
@@ -17,12 +17,11 @@ def string_to_integer(input)
 end
 
 def string_to_signed_integer(input)
-  if input[0] == '-'
-    input.slice!(0)
-    0 - string_to_integer(input)
-  elsif input[0] == '+'
-    input.slice!(0)
-    string_to_integer(input)
+  case input[0]
+  when '-'
+    - string_to_integer(input[1..-1])
+  when '+'
+    string_to_integer(input[1..-1])
   else
     string_to_integer(input)
   end

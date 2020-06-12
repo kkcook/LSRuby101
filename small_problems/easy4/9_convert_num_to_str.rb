b9a2c68e33bdb7453e8778783c4bf738b5ad8d11
Karissa Cook <karissa.cook@me.com>
2020-06-12 15:37:31 -0700

    PEDAC complete

---
+++ small_problems/easy4/9_convert_num_to_str.rb
@@ -0,0 +1,13 @@
+# Convert Number to a String
+
+# P: convert pos int to string
+# E: at bottom, watch out for empty inputs maybe
+# D: array or hash of corresponding int/string pairs
+# A: break int into digit array, assign string pairs, join, return
+
+
+
+# Examples:
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'

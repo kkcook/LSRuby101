89dccd90fb4545919056381ce0e35323a211464f
Karissa Cook <karissa.cook@me.com>
2020-06-11 17:51:45 -0700

    Somewhat brute force solution one

--- small_problems/easy4/8_convert_str_to_signed.rb
+++ small_problems/easy4/8_convert_str_to_signed.rb
@@ -1,3 +1,4 @@
+
 # Convert string to signed number
 # Prob: string may now have a sign at beginning,
 #   return an int with a leading sign if necessary
@@ -7,14 +8,25 @@
 
 NUMBERS = {
   '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
-  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9 
- }.freeze
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}.freeze
 
-def string_to_integer(string_num)
- int_array = string_num.chars.map {|n| NUMBERS[n]}
- int_array.inject { |sum, digit| sum * 10 + digit }
+def string_to_integer(input)
+  int_array = input.chars.map { |n| NUMBERS[n] }
+  int_array.inject { |sum, digit| sum * 10 + digit }
 end
 
+def string_to_signed_integer(input)
+  if input[0] == '-'
+    input.slice!(0)
+    0 - string_to_integer(input)
+  elsif input[0] == '+'
+    input.slice!(0)
+    string_to_integer(input)
+  else
+    string_to_integer(input)
+  end
+end
 
 # Examples
 p string_to_signed_integer('4321') == 4321
